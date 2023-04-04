aaad43fc390a669f42dd11aa9fd70c684fb409b8
RISHAV ROUSHAN <67714030+RishavRoushan@users.noreply.github.com>
2023-04-04 14:21:33 +0530

    Create logic.js
    logic behind the working of tip calculator

---
+++ Tip Calcultor/logic.js
@@ -0,0 +1,33 @@
+const billInput =document.getElementById('billTotalInput')
+const tipInput = document.getElementById('tipInput')
+const numberOfPeopleDiv = document.getElementById('numberOfPeople')
+const perPersonTotalDiv = document.getElementById('perPersonTotal')
+
+let numberOfPeople = Number(numberOfPeopleDiv.innerText)   
+
+const calculateBill = () => {
+    
+    const bill = Number(billInput.value)
+    const tipPercentage = Number(tipInput.value)/100
+    const tipAmount = bill*tipPercentage
+    const total = tipAmount+bill
+    const perPersonTotal = total/numberOfPeople
+    perPersonTotalDiv.innerText= `Rs.${perPersonTotal.toFixed(2)}`  //to fixed the decimal
+}
+
+const increasePeople = () => {
+    numberOfPeople+=1;
+    numberOfPeopleDiv.innerText=numberOfPeople
+    calculateBill();
+
+}
+
+const decreasePeople =() => {
+    if(numberOfPeople<=1)
+    return
+    else{
+        numberOfPeople-=1;
+        numberOfPeopleDiv.innerText=numberOfPeople
+    }
+    calculateBill();
+}
